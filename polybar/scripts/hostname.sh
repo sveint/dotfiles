ad97c6a2d96fb5a7a3afc5c55e6138ef74d4e7fa
Svein Tore Koksrud Seljebotn <s.t.seljebotn@medisin.uio.no>
2020-10-23 09:17:44 +0200

    Adjustments

--- polybar/scripts/hostname.sh
+++ polybar/scripts/hostname.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -ue
 
-HOSTNAME=$(curl -4 -s ifconfig.co/json | python -c "import json; a = open('/dev/stdin').read(); print '.'.join(json.loads(a).get('hostname', 'N/A').split('.')[-2:]) if a else 'offline'")
+HOSTNAME=$(curl -4 -s ifconfig.co/json | python -c "import json; a = open('/dev/stdin').read(); print '.'.join(json.loads(a).get('hostname', 'N/A').split('.')[-2:]) if '{\"ip\":' in a else 'offline'")
 echo "$HOSTNAME"
