e63e13d4fa80798620717e3c09ee45b18625190f
sveint <sveint@gmail.com>
2021-11-24 12:38:38 +0100

    Various adjustments

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,13 +6,15 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $(id -u) -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
 
-MAIN_MONITOR=$(polybar -m | grep primary | cut -f1 -d':')
-MONITOR=${MAIN_MONITOR} polybar -c ~/dotfiles/polybar/config.ini main &
-polybar -m | grep -v primary | cut -f1 -d':' | while read line ; do
+PRIMARY_MONITOR=$(polybar -m | grep primary | cut -f1 -d':')
+PRIMARY_MONITOR=${PRIMARY_MONITOR:-eDP-1}
+echo $PRIMARY_MONITOR
+MONITOR=${PRIMARY_MONITOR} polybar -c ~/dotfiles/polybar/config.ini main &
+polybar -m | grep -v ${PRIMARY_MONITOR} | cut -f1 -d':' | while read line ; do
    MONITOR=${line} polybar -c ~/dotfiles/polybar/config.ini extra &
 done
 
